3bc3872c9d86ad07ee0b05e7f86b18cc40c74f29
Pomidorchik03 <london-266@mail.ru>
2025-09-05 22:03:02 +0700

    Third commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 
 int main(int, char **) {
-    std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world!" << version() << std::endl;
     return 0;
 }
